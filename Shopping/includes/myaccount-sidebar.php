28a4bae9ab96cdf58c6bce5e5acc4f603a3e82b6
fatmakaradag <64332825+fatmakaradag@users.noreply.github.com>
2022-05-27 15:03:20 +0300

    Add files via upload

---
+++ Shopping/includes/myaccount-sidebar.php
@@ -0,0 +1,20 @@
+	<div class="col-md-4">
+					<!-- checkout-progress-sidebar -->
+<div class="checkout-progress-sidebar ">
+	<div class="panel-group">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+		    	<h4 class="unicase-checkout-title">Odeme Ilerlemeniz</h4>
+		    </div>
+		    <div class="panel-body">
+				<ul class="nav nav-checkout-progress list-unstyled">
+					<li><a href="my-account.php">Hesabım</a></li>
+					<li><a href="bill-ship-addresses.php">Teslimat / Fatura Adresi</a></li>
+					<li><a href="order-history.php">Siparis Gecmisi</a></li>
+					<li><a href="pending-orders.php">Odeme Bekleyen Siparis</a></li>
+				</ul>		
+			</div>
+		</div>
+	</div>
+</div> 
+<!-- checkout-progress-sidebar -->				</div>
